1796f026bf4a0d38beb95788a19bea68837176e7
Kenneth Ball <ballkenneth@gmail.com>
2015-10-02 10:51:05 -0400

    Update blasst_internal.m

--- blasst_internal.m
+++ blasst_internal.m
@@ -24,7 +24,27 @@ function [y,x] = blasst_internal(x,f,r,sR,scale,rez,manualOffset)
 % y     the [1,N] time course of signal features removed from x.
 % x     the [1,N] transformed signal.
 %
-% AUTHOR: Kenneth Ball, 2015
+% AUTHOR: Kenneth Ball, 2015.
+% 
+% IF YOU FIND BLASST USEFUL IN YOUR WORK, PLEASE CITE:
+%
+% Ball, K. R., Hairston, W. D., Franaszczuk, P. J., Robbins, K. A., 
+% BLASST: Band Limited Atomic Sampling with Spectral Tuning with 
+% Applications to Utility Line Noise Filtering, [Under Review].
+%
+% Copyright 2015 Kenneth Ball
+% 
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+% 
+%     http://www.apache.org/licenses/LICENSE-2.0
+% 
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
 
 
 
